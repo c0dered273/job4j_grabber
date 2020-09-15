567e0c99025736735145192e2d78433541e68216
c0dered273 <okaschuk@gmail.com>
2020-09-15 11:04:44 +0400

    SqlRuParse [#288523]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -1,10 +1,9 @@
-package ru.job4j.html;
+package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.posts.Post;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -16,31 +15,41 @@ import java.time.temporal.ChronoField;
 import java.util.*;
 
 
-public class SqlRuParse {
-    private final String url;
-    private final String topicQuery;
-    private final String descQuery;
+public class SqlRuParse implements Parse {
 
-    public SqlRuParse(String url, String topicQuery, String descQuery) {
-        this.url = url;
-        this.topicQuery = topicQuery;
-        this.descQuery = descQuery;
-    }
-
-    public List<Post> parse(int pages) {
+    @Override
+    public List<Post> list(String link) {
         List<Post> rsl = new ArrayList<>();
-        for (int i = 1; i <= pages; i++) {
-            String pageUrl = i == 1 ? url : url + "/" + i;
-            Elements topics = parseUrl(pageUrl, topicQuery);
-            if (topics != null) {
-                List<Post> post = parseElements(topics);
-                rsl.addAll(post);
+        String topicQuery = ".postslisttopic";
+        Elements elements = getElements(link, topicQuery);
+        if (elements != null) {
+            for (Element el : elements) {
+                String href = el.child(0).attr("href");
+                Post newPost = detail(href);
+                if (newPost != null) {
+                    rsl.add(newPost);
+                }
             }
         }
         return rsl;
     }
 
-    private Elements parseUrl(String url, String query) {
+    @Override
+    public Post detail(String link) {
+        Post rsl = null;
+        String detailsQuery = ".msgTable";
+        Elements details = getElements(link, detailsQuery);
+        if (details != null && details.size() > 0) {
+            String name = details.first().getElementsByClass("messageHeader").textNodes().get(0).text();
+            String desc = details.first().getElementsByClass("msgBody").get(1).text();
+            String date = details.first().getElementsByClass("msgFooter").textNodes().get(0).text();
+            date = date.substring(0, date.lastIndexOf("["));
+            rsl = new Post(link, name, desc, parseDate(date));
+        }
+        return rsl;
+    }
+
+    private Elements getElements(String url, String query) {
         Document doc = null;
         try {
             doc = Jsoup.connect(url).get();
@@ -83,27 +92,6 @@ public class SqlRuParse {
         }
         LocalTime localTime = LocalTime.parse(tm, timeFormatter);
         rsl = LocalDateTime.of(localDate, localTime);
-        return rsl;
-    }
-
-    private List<Post> parseElements(Elements elements) {
-        List<Post> rsl = new ArrayList<>();
-        for (Element el : elements) {
-            if (el.childrenSize() > 5) {
-                Elements childEls = el.child(1).children();
-                if (childEls.size() > 0) {
-                    String href = childEls.first().attr("href");
-                    String name = childEls.first().text();
-                    String date = el.child(5).text();
-                    String desc = "";
-                    Elements descEls = parseUrl(href, descQuery);
-                    if (descEls != null) {
-                        desc = descEls.get(1).text();
-                    }
-                    rsl.add(new Post(href, name, desc, parseDate(date)));
-                }
-            }
-        }
         return rsl;
     }
 }
